84dd729d72276dd9d3bfa1166d0c9f870dfd80cb
QiaowenYoung <17307130294@fudan.edu.cn>
2020-01-02 14:08:04 +0800

    softmax vectorized loss

--- assignment1/cs231n/classifiers/softmax.py
+++ assignment1/cs231n/classifiers/softmax.py
@@ -48,7 +48,7 @@ def softmax_loss_naive(W, X, y, reg):
     loss /= N
     dW /= N
     loss += 0.5 * reg * np.sum(W * W)
-    dW += reg *W
+    dW += reg * W
     # *****END OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
 
     return loss, dW
@@ -73,7 +73,21 @@ def softmax_loss_vectorized(W, X, y, reg):
     # *****START OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
 
     pass
-
+    N = X.shape[0]
+    scores = X.dot(W)
+    scores -= scores.max(axis = 1).reshape(-1, 1)
+    exp_sum = np.sum(np.exp(scores), axis = 1)
+    margin = -scores[range(N), y] + np.log(exp_sum)
+    loss += np.sum(margin)
+    
+    scores = np.exp(scores) / exp_sum.reshape(-1, 1)
+    scores[range(N), y] -= 1
+    dW += (X.T).dot(scores)
+    
+    loss /= N
+    dW /= N
+    loss += 0.5 * reg * np.sum(W * W)
+    dW += reg * W
     # *****END OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
 
     return loss, dW
